8f1d0f75bf9e2d962fe5515d9446ab3ec72f41d3
Arun Vishnu K <arunvishnuk008@gmail.com>
2025-10-07 00:39:24 +0530

    github pages changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'the-red-abode' title`, () => {
+  it(`should have the 'crimson-sunrise' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('the-red-abode');
+    expect(app.title).toEqual('crimson-sunrise');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, the-red-abode');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, crimson-sunrise');
   });
 });
